2661e2df5b6f0fd2e50cfe9a192eac51dd05e80f
Kamsi Rodrick <148686197+RodrickKamsiyonna@users.noreply.github.com>
2026-09-09 12:48:27 +0100

    Update jepa.py

--- jepa.py
+++ jepa.py
@@ -117,8 +117,7 @@ class JEPA(nn.Module):
         """Compute the cost between predicted embeddings and goal embeddings."""
         pred_emb = info_dict["predicted_emb"]  # (B,S, T-1, dim)
         goal_emb = info_dict["goal_emb"]  # (B, S, T, dim)
-        goal_emb = goal_emb[..., -1:, :].expand_as(pred_emb)
-
+        goal_emb = goal_emb[..., -1:, :].unsqueeze(-2).expand_as(pred_emb)
         # return last-step cost per action candidate
         cost = F.mse_loss(
             pred_emb[..., -1:, :],
